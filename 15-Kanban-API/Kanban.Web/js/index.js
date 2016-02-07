2b85a0a592f291d93c11fa5c276fb83c43fdbb1f
Jun Kang <jkang027@yahoo.com>
2016-02-06 22:51:32 -0800

    finished base assignment

--- 15-Kanban-API/Kanban.Web/js/index.js
+++ 15-Kanban-API/Kanban.Web/js/index.js
@@ -4,41 +4,58 @@ angular.module('Kanban').value('apiUrl', 'http://localhost:50026/api');
 
 angular.module('Kanban').controller('IndexController', function ($scope, $resource, apiUrl) {
 
-    var ListResource = $resource(apiUrl + '/lists/:listId', { listId: '@ListId' }, {
+    var CardResource = $resource(apiUrl + '/cards/:cardId', { cardId: '@CardId' }, {
         'cards': {
-            url: apiUrl + '/lists/:listId/cards',
             method: 'GET',
+            url: apiUrl + '/lists/:listId/cards',
             isArray: true
         }
     });
+    var ListResource = $resource(apiUrl + '/lists/:listId', { listId: '@ListId' });
 
-    var CardResource = $resource(apiUrl + '/cards/:cardId', { cardId: '@CardId' });
-
-    function activate() {
-        ListResource.query(function (data) {
-            $scope.lists = data;
-
-            $scope.lists.forEach(function (list) {
-                list.cards = ListResource.cards({ listId: list.ListId });
-            });
-        });
-    }
-
-    $scope.newList = {};
+    $scope.data = {
+        newList: {},
+        newCard: {}
+    };
 
     $scope.addList = function () {
-        ListResource.save($scope.newList, function () {
+        ListResource.save($scope.data.newList, function (data) {
             alert('Save Successful');
             activate();
         });
     };
 
-    $scope.deleteList = function (list) {
-        list.$delete(function () {
+    $scope.addCard = function (list) {
+        list.newCard.ListId = list.ListId;
+        CardResource.save(list.newCard, function (data) {
+            alert('Save Successful');
+            activate();
+        });
+    };
+
+    $scope.removeList = function (list) {
+        list.$remove(function (data) {
+            alert('Delete Successful');
+            activate();
+        });
+    };
+
+    $scope.removeCard = function (card) {
+        card.$remove(function (data) {
             alert('Delete Successful');
             activate();
         });
     };
 
     activate();
+
+    function activate() {
+        ListResource.query(function (data) {
+            $scope.data.lists = data;
+
+            $scope.data.lists.forEach(function (list) {
+                list.cards = CardResource.cards({ listId: list.ListId });
+            });
+        });
+    }
 });
